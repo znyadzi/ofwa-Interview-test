becb2f8e561bb0b6f6757faa8b04e58edf8f8459
znyadzi <125977854+znyadzi@users.noreply.github.com>
2025-02-01 02:29:45 +0000

    Database Creation
    Created the database table to hold our galamsey site data

--- galamsey_DStore/DbPopulate/admin.py
+++ galamsey_DStore/DbPopulate/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from .models import GSiteData  # Import the GSiteData model
+
 # Register your models here.
+
+#Ensuring that the site is not registered twice and give an error, we always check if the site is registered
 if not admin.site.is_registered(User):
-    admin.site.register(User)
+    admin.site.register(User)      #Register the user model
+if not admin.site.is_registered(GSiteData):
+    admin.site.register(GSiteData)  #Register the GSiteData model
--- galamsey_DStore/DbPopulate/models.py
+++ galamsey_DStore/DbPopulate/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class GSiteData(models.Model):
+    Town = models.CharField(max_length=100)
+    Region = models.CharField(max_length=100)
+    Number_of_Galamsay_Sites = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Town} ({self.Region}) - {self.Number_of_Galamsay_Sites} sites"
